58b267e9e2d680c94c4329cc00821910e0f3f0cb
extrasex3000 <ilyaha00@gmail.com>
2024-08-31 18:17:43 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Dziękujemy za wiadomość! Skontaktujemy się z Tobą wkrótce.');
+});
